4e1d167a60475800e4a3c6209aabcb531ff5e807
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-12 16:13:36 -0300

    Map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css';
 import mapMarkerImg from '../images/icon.svg';
@@ -22,7 +24,16 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div />
+      <Map
+        center={[-23.611679, -46.6801928]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
       <Link to="/" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
